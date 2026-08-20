dfdbcfec8e278e63230bc9ec6bf02cc0da96655b
777ernest888-oss <777ernest888@gmail.com>
2026-08-20 13:30:38 +0300

    Add files via upload

---
+++ src/db.js
@@ -0,0 +1,8 @@
+﻿const Database = require('better-sqlite3');
+const fs = require('fs');
+const path = require('path');
+const dbPath = process.env.DB_PATH || './data/parts.db';
+fs.mkdirSync(path.dirname(dbPath), { recursive: true });
+const db = new Database(dbPath);
+db.exec(fs.readFileSync(path.join(__dirname, 'db/schema.sql'), 'utf8'));
+module.exports = db;
